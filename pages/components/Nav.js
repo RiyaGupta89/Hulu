b4215c2a570da166994b9c05ddee4b1cb07e09b8
Riya Gupta git config --global user.email riyag0841@gmail.com <riyag0841@gmail.com>
2021-08-09 06:29:56 +0530

    routes added:

--- pages/components/Nav.js
+++ pages/components/Nav.js
@@ -1,13 +1,19 @@
 import React from 'react'
 import requests from '../../Utils/requests'
+import {useRouter} from "next/router"
 
 function Nav() {
+    const router = useRouter()
+   
     return (
         <nav className="relative">
         <div className="flex px-10 sm:px-20 text-2xl whitespace-nowrap space-x-10 sm:space-x-20 overflow-x-scroll scrollbar-hide">
             {Object.entries(requests).map(([key, {title, url}]) => {
                 return(
-                    <h2 key={key} className="last:pr-24 cursor-pointer transition duration-100 transform hover:scale-125 hover:text-white active:text-red-400">{title}</h2>
+                        <h2 key={key} className="last:pr-24 cursor-pointer transition duration-100 transform hover:scale-125 hover:text-white active:text-red-400" onClick={(e) => {
+                            e.preventDefault()
+            router.push(`/genre=${key}`);
+    }}>{title}</h2>
                 )
             })}
         </div>
